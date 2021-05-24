cf0f0e873ea6375c0da3d8812b9923b13f4ba9f0
nishitha13 <77734373+nishitha13@users.noreply.github.com>
2021-05-24 21:10:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var database;
+
+var gameState = 0;
+
+var form, player, game;
+
+var playerCount = 0;
+
+var allPlayers;
+
+var distance = 0;
+
+var car1, car2, car3, car4, cars;
+var track,car1Image,car2Image,car3Image,car4Image;
+var xVel, yVel;
+function preload(){
+track = loadImage("track.jpg");
+car1Image = loadImage("car1.png");
+car2Image = loadImage("car2.png");
+car3Image = loadImage("car3.png");
+car4Image = loadImage("car4.png");
+
+}
+
+function setup(){
+    database = firebase.database();
+
+    createCanvas(displayWidth-20,displayHeight-30); 
+    
+    xVel = 0;
+    yVel = 0;
+    
+    game  = new Game();
+    game.getState();
+    game.start();
+ 
+}
+
+function draw(){
+    //background("white");
+    if(playerCount === 4){
+        game.update(1);
+    }
+
+    if(gameState===1){
+        clear();
+        game.play();
+    }
+
+    if(gameState===2){
+        game.end();
+    }
+}
+
