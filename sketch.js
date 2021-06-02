2ed8ce5a1330891844d21e364f83899d092c949a
nishitha13 <77734373+nishitha13@users.noreply.github.com>
2021-06-02 21:30:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,13 +13,28 @@ var distance = 0;
 var car1, car2, car3, car4, cars;
 var track,car1Image,car2Image,car3Image,car4Image;
 var xVel, yVel;
-function preload(){
-track = loadImage("track.jpg");
-car1Image = loadImage("car1.png");
-car2Image = loadImage("car2.png");
-car3Image = loadImage("car3.png");
-car4Image = loadImage("car4.png");
 
+var finishedPlayers=0, obstacles, obstacle, obstacleImage;
+var carSound, i;
+var passedFinish;
+var bronze_img;
+var silver_img;
+var gold_img;
+
+
+function preload(){
+    track = loadImage("track.jpg");
+    car1Image = loadImage("car1.png");
+    car2Image = loadImage("car2.png");
+    car3Image = loadImage("car3.png");
+    car4Image = loadImage("car4.png");
+
+    obstacleImage = loadImage("f1.png");
+    ground = loadImage("ground.png");
+    bronze_img = loadImage("bronze.png");
+    silver_img = loadImage("Silver.png");
+    gold_img = loadImage("gold.png");
+    carSound=loadSound("sliding.mp3")
 }
 
 function setup(){
@@ -33,22 +48,41 @@ function setup(){
     game  = new Game();
     game.getState();
     game.start();
- 
+
+    obstacles = createGroup();
+    
+    //PLACING OBSTACLES AT RANDOM POSITIONS
+    for(i=0;i<5;i++)
+    {
+        w=random(200,950);
+        h=random(-height*4,height-300);
+        obstacle = createSprite(w,h,10,10);
+        obstacle.addImage("obstacle",obstacleImage);
+        obstacles.add(obstacle);
+    }
 }
 
 function draw(){
     //background("white");
-    if(playerCount === 4){
+    if(playerCount === 4 && finishedPlayers === 0){
         game.update(1);
     }
 
-    if(gameState===1){
+   
+    if(gameState===1 && passedFinish === false){
         clear();
         game.play();
     }
 
-    if(gameState===2){
-        game.end();
+    //end the game
+    if (finishedPlayers === 4) {
+        game.update(2);
+        //gameState = 2;
+    }
+
+    if(gameState===2 && finishedPlayers === 4){
+        
+        //game.end();
+        game.displayRanks();
     }
 }
-
